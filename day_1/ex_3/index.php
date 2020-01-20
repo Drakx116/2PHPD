6fd510a06b1d68c75e52a73a301570edbe48b912
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-20 13:32:22 +0100

    Add Day 1 Exercice 3

---
+++ day_1/ex_3/index.php
@@ -0,0 +1,21 @@
+<a href="../index.php"> Return </a>
+<h2> Display car registrations values </h2>
+
+<?php
+
+    foreach (range('A', 'Z') as $l1) {
+        foreach (range('A', 'Z') as $l2) {
+            foreach (range(0, 9) as $n1) {
+                foreach (range(0, 9) as $n2) {
+                    foreach (range(0, 9) as $n3) {
+                        foreach (range('A', 'Z') as $l3) {
+                            foreach (range('A', 'Z') as $l4) {
+                                echo $l1 . $l2 . '-' . $n1 . $n2 . $n3 . '-' . $l3 . $l4 . '<br />';
+                            }
+                        }
+                        die;
+                    }
+                }
+            }
+        }
+    }
--- day_1/index.php
+++ day_1/index.php
@@ -1,4 +1,5 @@
 <ul>
     <li><a href="./ex_1/"> Exercice 1 </a></li>
     <li><a href="./ex_2/"> Exercice 2 </a></li>
+    <li><a href="./ex_3/"> Exercice 3 </a></li>
 </ul>
