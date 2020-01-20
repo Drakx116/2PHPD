1f2caead72ac7016a62bc23c81d887ded1c134f5
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-20 13:04:23 +0100

    Add Day 1 First Exercice

---
+++ day_1/index.php
@@ -0,0 +1,28 @@
+<?php
+    $age = 36;
+    $gender = 'm';
+
+    //////////////////////////
+    echo '<b> If / Else </b>';
+    echo '<br />';
+    //////////////////////////
+
+    // If / Else
+    if($age >= 31 && $age <= 50 && $gender === 'm') {
+        echo 'You\'re a man between 31 and 50 years old';
+    }
+    else {
+        echo 'You\'re not a man between 31 and 50 years old';
+    }
+
+    //////////////////////////
+    echo '<br />';
+    echo '<br />';
+    echo '<b> Ternary </b>';
+    echo '<br />';
+    //////////////////////////
+
+    // Ternary
+    echo ($age >= 31 && $age <= 50 && $gender === 'm')
+        ? 'You\'re a man between 31 and 50 years old'
+        : 'You\'re not a man between 31 and 50 years old';
