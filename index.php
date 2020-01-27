42dcf1a2a8458978c24286407536726433991f49
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-27 15:08:32 +0100

    Add project link

--- index.php
+++ index.php
@@ -4,4 +4,5 @@
     <li><a href="./day_1"> Day 1 </a></li>
     <li><a href="./day_2"> Day 2 </a></li>
     <li><a href="./day_3"> Day 3 </a></li>
+    <li><a href="./project/"> Project  </a></li>
 </ul>
