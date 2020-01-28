ca6beeebd57669982625bec88bd74528a8bae3e9
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-28 11:30:32 +0100

    Add htmlspecialchars on echo

--- day_2/ex_1/post.php
+++ day_2/ex_1/post.php
@@ -1,9 +1,9 @@
 <div>
-    <?php echo  'Pseudo : '. ($_POST['pseudo'] ?? '') . '<br /><br />'; ?>
-    <?php echo  'Email : '. ($_POST['email'] ?? '') . '<br /><br />'; ?>
-    <?php echo  'Address : '. ($_POST['address'] ?? '') . '<br /><br />'; ?>
-    <?php echo  'PC : '. ($_POST['pc'] ?? '') . '<br /><br />'; ?>
-    <?php echo  'City : '. ($_POST['city'] ?? '') . '<br /><br />'; ?>
+    <?php echo  'Pseudo : '. (htmlspecialchars($_POST['pseudo']) ?? '') . '<br /><br />'; ?>
+    <?php echo  'Email : '. (htmlspecialchars($_POST['pseudo']) ?? '') . '<br /><br />'; ?>
+    <?php echo  'Address : '. (htmlspecialchars($_POST['address']) ?? '') . '<br /><br />'; ?>
+    <?php echo  'PC : '. (htmlspecialchars($_POST['pc']) ?? '') . '<br /><br />'; ?>
+    <?php echo  'City : '. (htmlspecialchars($_POST['city']) ?? '') . '<br /><br />'; ?>
 </div>
 
 <a href="./index.php"> Return </a>
