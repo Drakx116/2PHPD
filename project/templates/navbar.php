310696e17923eb1cd967afd9bf72e0e1bf9232a0
Drakx116 <alexandre2.guyot@gmail.com>
2020-01-28 16:17:38 +0100

    Update navbar

--- project/templates/navbar.php
+++ project/templates/navbar.php
@@ -1,6 +1,7 @@
 <nav>
     <ul>
         <li><a href="./index.php"> Home </a></li>
-        <li><a href="./article.php"> Article </a></li>
+        <li><a href="./article.php"> Articles </a></li>
+        <li><a href="./meme.php"> Memes </a></li>
     </ul>
 </nav>
