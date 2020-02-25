4961d84a3aa389a8e5c0343707be124c57ef9d90
Drakx116 <alexandre2.guyot@gmail.com>
2020-02-25 10:53:24 +0100

    Update daily links

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <h2> 2PHPD Labs </h2>
 
 <ul>
-    <li><a href="./day_1"> Day 1 </a></li>
-    <li><a href="./day_2"> Day 2 </a></li>
-    <li><a href="./day_3"> Day 3 </a></li>
+    <li><a href="./day_1"> Day 1 - Basics </a></li>
+    <li><a href="./day_2"> Day 2 - Forms</a></li>
+    <li><a href="./day_3"> Day 3 - Working with database</a></li>
+    <li><a href="./day_4"> Day 4 - Blog </a></li>
     <li><a href="./project/"> Project  </a></li>
 </ul>
