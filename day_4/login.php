cd503ef96a4c1d5d7c7ae6a738b4fb33bf41d2aa
Drakx116 <alexandre2.guyot@gmail.com>
2020-02-25 16:51:38 +0100

    Add login

--- day_4/login.php
+++ day_4/login.php
@@ -1,12 +1,71 @@
-<!DOCTYPE html>
-<html>
-<head>
+<?php
+    session_start();
+    require_once './templates/navbar.php';
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Login </title>
 </head>
 
 <body>
-    <?php require_once './templates/navbar.php' ?>
-
     <h2> Login </h2>
+
+    <?php
+        $errors = [];
+        if(isset($_POST['form-login'])) {
+            $pseudo = $_POST['pseudo'] ?? null;
+            $plainPassword = $_POST['password'] ?? null;
+
+            if(!($pseudo && $plainPassword)) {
+                $errors[] = 'Missing parameters';
+            }
+
+            // Valid form params
+            if(!$errors) {
+                $getUserQuery = $db->prepare('SELECT * FROM users WHERE pseudo = ?');
+                $getUserQuery->execute([ $pseudo ]);
+
+                if(!$getUserQuery->rowCount()) {
+                    $errors[] = 'Cannot find any related user with this pseudo.';
+                }
+
+                // User found
+                if(!$errors) {
+                    $user = $getUserQuery->fetch(PDO::FETCH_ASSOC);
+
+                    if(!password_verify($plainPassword, $user['password'])) {
+                        $errors[] = 'Invalid credentials';
+                    }
+
+                    if(!$errors) {
+                        $_SESSION['pseudo'] = $user['pseudo'];
+                        header('Location: index.php');
+                    }
+                }
+            }
+        }
+    ?>
+
+    <form action="" method="POST">
+        <input type="text" name="pseudo" placeholder="Pseudo"><br>
+        <input type="password" name="password" placeholder="Password"><br>
+
+        <input type="submit" name="form-login" value="Register">
+    </form>
+    <br>
+
+    <div>
+        <?php
+        if($errors) {
+            echo '<b> Errors : '  . count($errors) .' </b><br><br>';
+            foreach ($errors as $error) {
+                echo $error . '<br>';
+            }
+        }
+        ?>
+    </div>
+
 </body>
 </html>
