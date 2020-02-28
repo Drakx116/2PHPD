96885770721acc4440943da02998fe8a79a3d17e
Drakx116 <alexandre2.guyot@gmail.com>
2020-02-28 08:35:42 +0100

    Init day 5

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     <li><a href="./day_2"> Day 2 - Forms</a></li>
     <li><a href="./day_3"> Day 3 - Working with database</a></li>
     <li><a href="./day_4"> Day 4 - Blog </a></li>
+    <li><a href="./day_5"> Day 5 - OOP </a></li>
     <li><a href="./project/"> Project  </a></li>
 </ul>
